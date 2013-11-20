0a67333e15b987ce7831b882e99902af274dacf5
joash xu <joash.xu@gmail.com>
2013-11-20 21:02:38 +0700

    Add deployment scripts

---
+++ deployment/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+LOGFILE=/home/ubuntu/webapps/tesseract/tesseract.log
+NUM_WORKERS=2
+USER=ubuntu
+
+# user/group to run as
+cd /home/ubuntu/webapps/tesseract/django-tesseract
+source /home/ubuntu/webapps/tesseract/tesseract/bin/activate
+exec python manage.py run_gunicorn --bind=127.0.0.1:8000 --settings=djangotesseract.settings 1>>$LOGFILE 2>>$LOGFILE
--- fabfile.py
+++ fabfile.py
@@ -1,55 +1,65 @@
 import contextlib
 from fabric.api import env, run, cd, sudo, put, require, settings, hide, puts
 from fabric.contrib import project, files
+from fabric.contrib.files import upload_template
+
 
 env.user = "ubuntu"
 env.hosts = ['ec2-23-21-150-134.compute-1.amazonaws.com']
 env.key_filename = ["demo.pem"]
-env.repo_url = 'git@github.com:setaris/django-tesseract.git'
+env.repo_url = 'git@github.com:joashxu/django-tesseract.git'
 
-env.root = "~/webapps/tesseract"
+env.root = "/home/ubuntu/webapps/tesseract"
 env.virtualenv = "%s/tesseractenv" % env.root
 env.project = "%s/django-tesseract" % env.root
+env.servicename = "setarisocr"
 
 
 def deploy():
     "Full deploy: push, buildout, and reload."
     push()
     update_dependencies()
+    update_services()
     reload()
 
 
 def push():
     "Push out new code to the server."
     with cd("%(project)s" % env):
-        sudo("git pull origin master")
+        run("git pull origin master")
+
+
+def update_services():
+    upload_template('./deployment/nginx.conf',
+        '/etc/nginx/sites-enabled/default', use_sudo=True)
+    upload_template('./deployment/service.conf',
+        '/etc/init/setarisocr.conf', use_sudo=True)
 
 
 def update_dependencies():
-    sudo("%(virtualenv)s/bin/pip install -r %(project)s/requirements.txt" % env)
+    run("%(virtualenv)s/bin/pip install -r %(project)s/requirements.txt" % env)
 
 
 def reload():
-    sudo("invoke-rc.d apache2 reload")
+    sudo("stop setarisocr")
+    sudo("start setarisocr")
+    sudo('/etc/init.d/nginx reload')
 
 
 def setup():
-    sudo("mkdir -p %(root)s" % env)
-    sudo("aptitude update")
-    sudo("aptitude -y install git-core python-dev python-setuptools "
-        "build-essential subversion mercurial apache2 "
-        "libapache2-mod-wsgi")
+    # run("mkdir -p %(root)s" % env)
+    # sudo("aptitude update")
+    # sudo("aptitude -y install git-core python-dev python-setuptools "
+    #     "build-essential subversion mercurial nginx"
+    #     "libjpeg62 libjpeg62-dev zlib1g-dev libfreetype6 libfreetype6-dev "
+    #     "ghostscript imagemagick "
+    #     "libtesseract-dev")
 
-    sudo("easy_install virtualenv")
-    sudo("virtualenv %(virtualenv)s" % env)
-    sudo("%(virtualenv)s/bin/pip install -U pip" % env)
+    # sudo("easy_install virtualenv")
+    # run("virtualenv %(virtualenv)s" % env)
+    # run("%(virtualenv)s/bin/pip install -U pip" % env)
 
-    with cd("~/webapps/tesseract"):
-        sudo("git clone %(repo_url)s" % env)
-
-    with cd("/etc/apache2"):
-        sudo("rm -rf apache2.conf conf.d/ httpd.conf magic mods-* sites-* ports.conf")
-        sudo("ln -s %(project)s/apache2.conf ." % env)
-        sudo("mkdir -m777 -p /var/www/.python-eggs")
+    #with cd("~/webapps/tesseract"):
+    #    run("git clone %(repo_url)s" % env)
 
     deploy()
